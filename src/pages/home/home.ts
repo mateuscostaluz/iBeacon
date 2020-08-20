017c4bfffedb4404b8d3fc5253332a9f8d4d05e2
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-08-19 23:41:26 -0300

    Simple update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IBeacon } from '@ionic-native/ibeacon';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private ibeacon: IBeacon) {
 
   }
 
+  ionViewWillEnter() {
+    // Request permission to use location on iOS
+    this.ibeacon.requestAlwaysAuthorization();
+    // create a new delegate and register it with the native layer
+    let delegate = this.ibeacon.Delegate();
+
+    // Subscribe to some of the delegate's event handlers
+    delegate.didRangeBeaconsInRegion()
+      .subscribe(
+        data => console.log('didRangeBeaconsInRegion: ', data),
+        error => console.error()
+      );
+    delegate.didStartMonitoringForRegion()
+      .subscribe(
+        data => console.log('didStartMonitoringForRegion: ', data),
+        error => console.error()
+      );
+    delegate.didEnterRegion()
+      .subscribe(
+        data => {
+          console.log('didEnterRegion: ', data);
+        }
+      );
+
+    let beaconRegion = this.ibeacon.BeaconRegion('presensor','fda50693-a4e2-4fb1-afcf-c6eb07647825');
+
+    this.ibeacon.startMonitoringForRegion(beaconRegion)
+      .then(
+        () => console.log('Native layer received the request to monitoring'),
+        error => console.error('Native layer failed to begin monitoring: ', error)
+      );
+  }
 }
